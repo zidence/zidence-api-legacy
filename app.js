a815b176f40074a74655bf77c365207993b9d3dc
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-10-26 17:47:54 +0700

    Add auth route and delete view engine setting

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ const cors = require('cors')
 
 const indexRouter = require('./src/routes/index')
 const usersRouter = require('./src/routes/users')
+const authRouter = require('./src/routes/auth')
 
 const app = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
 
 app.use(cors())
 app.use(logger('dev'))
@@ -24,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-
+app.use('/auth', authRouter)
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404))
