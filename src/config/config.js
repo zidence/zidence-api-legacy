afb692fc0f29ddb01fdb7266879832ac1a5ff432
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-10-27 11:38:59 +0700

    Set heroku DATABASE_URL

--- src/config/config.js
+++ src/config/config.js
@@ -16,6 +16,7 @@ module.exports = {
     migrationStorageTableName: 'sequelize_meta',
   },
   production: {
+    use_env_variable: process.env.DATABASE_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
